c7bd9240a4b112fcd9096a2738b2d117fcf6e556
RexCcx <liurunxiao233@gmail.com>
2023-06-20 21:45:39 +1000

    update data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
 // import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
+// import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   constructor(props) {
