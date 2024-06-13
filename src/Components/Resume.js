d51ae19d103e4e44c56fd98e338255f5f5f3361b
Runxiao Liu <liurunxiao233@gmail.com>
2024-06-13 21:51:40 +1000

    Regular update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -14,20 +14,7 @@ class Resume extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const skillmessage = this.props.data.skillmessage;
-    const education = this.props.data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
-    
+    const skillmessage = this.props.data.skillmessage; 
     const work = this.props.data.work.map(function (work) {
       return (
         <div key={work.company}>
@@ -65,24 +52,21 @@ class Resume extends Component {
       );
     });
 
+    const education = this.props.data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+
     return (
       <section id="resume">
-        <Slide left duration={1300}>
-          <div className="row education">
-            <div className="three columns header-col">
-              <h1>
-                <span>Education</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">{education}</div>
-              </div>
-            </div>
-          </div>
-        </Slide>
-
         <Slide left duration={1300}>
           <div className="row work">
             <div className="three columns header-col">
@@ -108,6 +92,22 @@ class Resume extends Component {
 
               <div className="bars">
                 <ul className="skills">{skills}</ul>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Education</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{education}</div>
               </div>
             </div>
           </div>
